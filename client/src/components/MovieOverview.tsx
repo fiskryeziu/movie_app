28100321ffde6c1a64e321a00b18d76492d30a4e
fiskryeziu <fiskryeziu@gmail.com>
2023-11-12 20:11:50 +0100

    add skeleton onLoad, responsive style

--- client/src/components/MovieOverview.tsx
+++ client/src/components/MovieOverview.tsx
@@ -3,13 +3,17 @@ import poster from "../assets/movie.jpg"
 
 const MovieOverview = () => {
   return (
-    <div className="w-full flex my-10 flex-col lg:flex-row gap-10 ">
-      <div className="w-1/4 h-full ">
-        <img src={poster} alt="" className="object-cover" />
+    <div className="w-full flex my-10 flex-col lg:flex-row gap-10">
+      <div className="w-full lg:w-1/4 h-full">
+        <img
+          src={poster}
+          alt=""
+          className="object-cover h-80 sm:h-96  mx-auto lg:mx-0"
+        />
       </div>
-      <div className="flex flex-col w-3/4">
+      <div className="flex flex-col w-full lg:w-3/ items-center lg:items-start">
         <p className="text-xl uppercase text-primary">Overview</p>
-        <p className="text-2xl uppercase max-w-lg mb-10">
+        <p className="text-2xl uppercase max-w-lg mb-10 text-center lg:text-left">
           Doctor Strange in the Multiverse of Madness (2022)
         </p>
         <p className="max-w-xl mb-10">
@@ -17,8 +21,8 @@ const MovieOverview = () => {
           traverses the mind-bending and dangerous alternate realities of the
           Multiverse to confront a mysterious new adversary.
         </p>
-        <div className="w-full flex">
-          <div className="w-1/2 flex-col space-y-5">
+        <div className="w-auto lg:w-full flex flex-col gap-5  lg:flex-row ">
+          <div className="w-auto lg:w-1/2 flex-col space-y-5">
             <div className="flex gap-2">
               <Film className="text-primary" />
               <span className="flex font-bold"> Genre: </span>
@@ -35,7 +39,7 @@ const MovieOverview = () => {
               <p>2022 </p>
             </div>
           </div>
-          <div className="w-1/2 flex-col space-y-5">
+          <div className="w-auto lg:w-1/2 flex-col space-y-5">
             <div className="flex gap-2">
               <Clock className="text-primary" />
               <span className="flex font-bold"> Duration: </span>
--- client/src/components/MoviePlay.tsx
+++ client/src/components/MoviePlay.tsx
@@ -1,24 +1,33 @@
-import { Play } from "lucide-react"
+import { Skeleton } from "./ui/skeleton"
+import { useState } from "react"
 
 const MoviePlay = () => {
+  const [loading, setLoading] = useState(true)
+  const handleLoader = () => {
+    setLoading(false)
+  }
   return (
     <>
-      <div>
+      <div className="w-full">
+        {loading && (
+          <Skeleton className="w-full lg:h-[540px] sm:h-auto md:h-[400px]" />
+        )}
         <iframe
           src="https://hqq.ac/e/T0lYTWNhcE1sQk53NDlJOGozRGIwZz09"
           allowFullScreen
           className="w-full aspect-video lg:h-[540px] sm:h-auto md:h-[400px]"
+          onLoad={handleLoader}
         />
       </div>
       <div className="flex gap-5 mb-10">
-        <button className="flex text-left rounded-md font-bold bg-secondary p-2 hover:bg-primary hover:duration-150">
-          Server 1 <Play />
+        <button className="flex text-left rounded-md font-bold bg-secondary p-2 hover:bg-primary hover:duration-150 text-sm sm:text-base">
+          Server 1
         </button>
-        <button className="flex text-left rounded-md font-bold bg-secondary p-2 hover:bg-primary hover:duration-150">
-          Server 2 <Play />
+        <button className="flex text-left rounded-md font-bold bg-secondary p-2 hover:bg-primary hover:duration-150 text-sm sm:text-base">
+          Server 2
         </button>
-        <button className="flex text-left rounded-md font-bold bg-secondary p-2 hover:bg-primary hover:duration-150">
-          Server 3 <Play />
+        <button className="flex text-left rounded-md font-bold bg-secondary p-2 hover:bg-primary hover:duration-150 text-sm sm:text-base">
+          Server 3
         </button>
       </div>
     </>
