6712e42c90b6e3692d745dacbd6c1abca9117eb7
fiskryeziu <fiskryeziu@gmail.com>
2023-11-16 19:41:16 +0100

    add dummy data for genre,popular,rated page.

--- client/src/pages/Genre.tsx
+++ client/src/pages/Genre.tsx
@@ -1,7 +1,19 @@
-import React from "react"
+import Card from "@/components/Card"
+import { useParams } from "react-router-dom"
 
 const Genre = () => {
-  return <div>Genre</div>
+  const { type } = useParams()
+
+  return (
+    <div className="flex flex-col mt-20">
+      <p className="text-xl text-primary font-bold pb-5 uppercase">{type}</p>
+      <div className="grid grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-5 lg:gap-10">
+        {Array.from({ length: 10 }, (_, x) => x).map((_, idx) => (
+          <Card key={idx} />
+        ))}
+      </div>
+    </div>
+  )
 }
 
 export default Genre
--- client/src/pages/Rated.tsx
+++ client/src/pages/Rated.tsx
@@ -1,7 +1,19 @@
+import Card from "@/components/Card"
 import React from "react"
 
 const Rated = () => {
-  return <div>Rated</div>
+  return (
+    <div className="flex flex-col mt-20">
+      <p className="text-xl text-primary font-bold pb-5 uppercase">
+        Highest-rated
+      </p>
+      <div className="grid grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-5 lg:gap-10">
+        {Array.from({ length: 10 }, (_, x) => x).map((_, idx) => (
+          <Card key={idx} />
+        ))}
+      </div>
+    </div>
+  )
 }
 
 export default Rated
