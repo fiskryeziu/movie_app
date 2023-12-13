765d6ea1c5ce74e2badb379aa09becd14f861bc2
fiskryeziu <fiskryeziu@gmail.com>
2023-12-13 18:15:38 +0100

    view counter

--- client/src/components/MoviePlay.tsx
+++ client/src/components/MoviePlay.tsx
@@ -1,21 +1,55 @@
 import { Movie } from "types"
 import { Skeleton } from "./ui/skeleton"
+import { trpc } from "@/trpc"
+import ReactPlayer from "react-player/streamable"
+import { useEffect } from "react"
 type TMovie = {
   movie: Movie
   isLoading: boolean
 }
+
 const MoviePlay = ({ movie, isLoading }: TMovie) => {
+  const { mutate } = trpc.movie.incrementView.useMutation({
+    onSuccess: () => {
+      console.log("success")
+    },
+    onError: () => {
+      console.log("error")
+    },
+  })
+
+  useEffect(() => {
+    const hasViewed = localStorage.getItem(`viewed_${movie.id}`)
+
+    // Only increment view count if the user has not viewed the movie before
+    if (!hasViewed && movie.id) {
+      mutate({
+        movieId: movie.id,
+      })
+    }
+  }, [movie.id, mutate])
+
   return (
     <>
       <div className="w-full">
         {isLoading ? (
           <Skeleton className="w-full lg:h-[540px] sm:h-auto md:h-[400px]" />
         ) : (
-          <iframe
-            src={movie.movieURL}
-            allowFullScreen
-            className="w-full aspect-video lg:h-[540px] sm:h-auto md:h-[400px]"
-          />
+          <div className="w-full h-0 relative pb-[56.250%]">
+            <ReactPlayer
+              url={movie.movieURL}
+              width="100%"
+              height="100%"
+              style={{
+                width: "100%",
+                height: "100%",
+                position: "absolute",
+                left: "0px",
+                top: "0px",
+                overflow: "hidden",
+              }}
+            />
+          </div>
         )}
       </div>
       <div className="flex gap-5 mb-10">
--- server/router/movie.ts
+++ server/router/movie.ts
@@ -27,4 +27,29 @@ export const movieRouter = router({
             return movie
         })
     ,
+
+    incrementView: publicProcedure
+        .input(z.object({ movieId: z.string() }))
+        .mutation(async ({ ctx, input }) => {
+            await ctx.prisma.movie.update({
+                where: {
+                    id: input.movieId
+                },
+                data: {
+                    viewCount: {
+                        increment: 1
+                    }
+                }
+
+            })
+
+        })
+    // TODO: movie view counter functionality
+    // so we can use to get popular.tsx page movies
+    // the view counter will get called after 3 seconds of watching 
+
+
+    // TODO: watched movies for the user
+    // api gets called after 30s of watching the movie
+    // the data will be saved in user watchedList field 
 });
